6d780a84eae9c4fcb3a9729359482d4c1e80fc39
Karthik D <karthik947@Karthiks-MacBook-Pro.local>
2023-08-23 21:09:17 +0530

    demo2 changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // SIMPLE QUEUE
 import Bull from "bull";
 import dotenv from "dotenv";
+import { promisify } from "util";
+const sleep = promisify(setTimeout);
 
 dotenv.config();
 const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
@@ -12,12 +14,32 @@ const redisOptions = {
 const burgerQueue = new Bull("burger", redisOptions);
 
 // REGISTER PROCESSER
-burgerQueue.process((payload, done) => {
-  console.log("Preparing the burger!");
-  setTimeout(() => {
-    console.log("Burger ready!");
+burgerQueue.process(async (payload, done) => {
+  try {
+    // STEP 1
+    payload.log("Grill the patty.");
+    payload.progress(20);
+    await sleep(5000);
+    // STEP 2
+    payload.log("Toast the buns.");
+    payload.progress(40);
+    await sleep(5000);
+    // STEP 3
+    payload.log("Add toppings.");
+    payload.progress(60);
+    await sleep(5000);
+    // STEP 4
+    payload.log("");
+    payload.log("Assemble layers.");
+    payload.progress(80);
+    await sleep(5000);
+    // STEP 5
+    payload.log("Burger ready.");
+    await payload.progress(100);
     done();
-  }, 4000);
+  } catch (err) {
+    done(err);
+  }
 });
 
 //ADD JOB TO THE QUEUE
