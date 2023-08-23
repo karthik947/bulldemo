2d0d2e00b520f398f1066f8f464d51b3010489a3
Karthik D <karthik947@Karthiks-MacBook-Pro.local>
2023-08-23 20:27:51 +0530

    demo1 changes

---
+++ index.js
@@ -0,0 +1,28 @@
+// SIMPLE QUEUE
+import Bull from "bull";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+const redisOptions = {
+  redis: { host: REDIS_HOST, port: REDIS_PORT, password: REDIS_PASSWORD },
+};
+
+// DEFINE QUEUE
+const burgerQueue = new Bull("burger", redisOptions);
+
+// REGISTER PROCESSER
+burgerQueue.process((payload, done) => {
+  console.log("Preparing the burger!");
+  setTimeout(() => {
+    console.log("Burger ready!");
+    done();
+  }, 4000);
+});
+
+//ADD JOB TO THE QUEUE
+burgerQueue.add({
+  bun: "🍔",
+  cheese: "🧀",
+  toppings: ["🍅", "🫒", "🥒", "🌶️"],
+});
